21b885a00334b44d8e241066f15883c9f5f567a2
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 15:11:42 -0300

    adicionado o service de ClienteService CRUD

---
+++ clientes/src/main/java/com/exercicio/clientes/service/ClienteService.java
@@ -0,0 +1,34 @@
+package com.exercicio.clientes.service;
+
+import com.exercicio.clientes.entity.Cliente;
+import com.exercicio.clientes.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
